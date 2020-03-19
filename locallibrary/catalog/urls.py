ac873e42447a5bc1f5f84a69bac4a5c2859d7d14
Anton <testvost971@gmail.com>
2020-03-19 23:17:47 +0300

    fourth

--- locallibrary/catalog/urls.py
+++ locallibrary/catalog/urls.py
@@ -3,5 +3,4 @@ from . import views
 
 urlpatterns = [
 	path("", views.index, name="index"),
-	path("src_frg/", views.search_fragment, name="src_frg"),
 ]
--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -24,39 +24,6 @@ def index(request):
         	'num_instances':num_instances,
         	'num_instances_available':num_instances_available,
         	'num_authors':num_authors,
-        	'fragment':'fragment',
         },
     )
 
-
-def search_fragment(request):
-	"""
-	Searchs and displays books that include given fragment in their titles
-	"""
-
-	#str method find but returns True or False
-	def find_bin(title, fragment):
-		res = title.lower().find(fragment)
-		if (res == -1):
-			return False
-		else:
-			return True
-	
-	#gets POST data
-	try:
-		request.method == "POST"
-	except:
-		pass
-	else:
-		fragment = request.POST["fragment"]
-
-	list_of_req_books = [book for book in Book.objects.all() if find_bin(book.title, fragment)]
-
-	return render(
-		request, 
-		"src_frg.html", 
-		context={
-			"list_of_req_books":list_of_req_books,
-			"fragment":fragment,
-		}, 
-	)
